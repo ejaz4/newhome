7d115e4d04282ff806eb375a29786e5759ba130a
ejaz4 <ejazali4@protonmail.ch>
2025-12-21 06:33:17 +0000

    feature(routing): fully setup routing
    - fully setup routing in-line with generouted conventions
      (https://github.com/oedotme/generouted#conventions)
    - nested layouts allow for routes to decorate themselves with the
      layouts of all their parents

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import App from "./App.jsx";
+import { Routes } from "@generouted/react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <Routes />
+  </StrictMode>,
 );
---
+++ src/pages/_app.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+const App = () => {
+  return (
+    <>
+      <p>hello</p>
+      <Outlet />
+    </>
+  );
+};
+
+export default App;
