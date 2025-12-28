958da216d2b5daf3cf3580c2c827b3b67c0199d9
ejaz4 <ejazali4@protonmail.ch>
2025-12-28 11:17:40 +0000

    feat(search): create search box
    - create search box as form
    - make search box lead to search page.

---
+++ src/pages/(app)/search.jsx
@@ -0,0 +1,13 @@
+import { SearchBox } from "../../_components/search/box";
+import { SearchResultsList } from "../../_components/search/results";
+
+const SearchResultsPage = () => {
+  return (
+    <div>
+      <SearchBox />
+      <SearchResultsList />
+    </div>
+  );
+};
+
+export default SearchResultsPage;
