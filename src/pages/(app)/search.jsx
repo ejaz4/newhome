0cde5907c2842bf2da04b02345d8650053210b79
ejaz4 <ejazali4@protonmail.ch>
2025-12-28 14:32:34 +0000

    fix(search): added query to the list of props with SearchResultsList
    - to pass down a hook from a parent down to a child

--- src/pages/(app)/search.jsx
+++ src/pages/(app)/search.jsx
@@ -1,11 +1,14 @@
+import { useSearchParams } from "react-router";
 import { SearchBox } from "../../_components/search/box";
 import { SearchResultsList } from "../../_components/search/results";
 
 const SearchResultsPage = () => {
+  const [searchParams] = useSearchParams();
+
   return (
     <div>
       <SearchBox />
-      <SearchResultsList />
+      <SearchResultsList query={searchParams.get("q")} />
     </div>
   );
 };
