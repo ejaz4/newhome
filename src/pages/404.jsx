b05ad1e6888397fbdd278bc3bcb14324e1cd3e36
ejaz4 <ejazali4@protonmail.ch>
2026-01-05 08:55:00 +0000

    chore(routing): made 404 page look more in-line with the rest of the website

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,27 @@
+import { FrownIcon } from "lucide-react";
+import { Header } from "../_components/header";
+
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <div className="bodyPadding">
+      <Header />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "2rem",
+          gap: "1rem",
+        }}
+      >
+        <h2>
+          <FrownIcon /> Page not found
+        </h2>
+        <p>This page couldn&apos;t be found.</p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
