82b42341a851f9fb024bd1c07ef7b1933995cedd
ejaz4 <ejazali4@protonmail.ch>
2026-01-05 08:52:46 +0000

    refactor(routing): removed placeholder route

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,6 @@ import { components, hooks, utils } from '@generouted/react-router/client'
 
 export type Path =
   | `/`
-  | `/bongus`
   | `/favourites`
   | `/listing/*`
   | `/listing/:id/gallery`
